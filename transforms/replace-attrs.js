0b837d118970975dbff96408107ab0f60784efdb
Kus Cámara <kcmr@users.noreply.github.com>
2020-01-03 20:10:08 +0100

    refactor(replace-attrs): improve legibility

--- transforms/replace-attrs.js
+++ transforms/replace-attrs.js
@@ -1,34 +1,46 @@
 import { htmlTaggedTemplateLiteral } from './filters';
 
-function replaceAttrs(source, tag, attrs) {
-  const tagWithAttrs = new RegExp(`<${tag}([^>]*)>`, 'g');
+class AttrReplacer {
+  constructor(parser, tag, attrs) {
+    this.parser = parser;
+    this.attrs = attrs;
+    this.tagWithAttrs = new RegExp(`<${tag}([^>]*)>`, 'g');
+    this.hasTag = this.nodeHasTag.bind(this);
+    this.tagWithNewAttrs = this.getTagWithNewAttrs.bind(this);
+  }
 
-  return source.replace(tagWithAttrs, (m) => {
-    for (const [from, to] of Object.entries(attrs)) {
-      m = m.replace(new RegExp(`${from}(\s*=)`, 'g'), (f) =>
-        f.replace(from, to)
-      );
-    }
+  nodeHasTag(path) {
+    return this.parser(path.node)
+      .toSource()
+      .match(this.tagWithAttrs);
+  }
 
-    return m;
-  });
+  getTagWithNewAttrs(path) {
+    const source = this.parser(path).toSource();
+    return this.replaceAttrs(source);
+  }
+
+  replaceAttrs(source) {
+    return source.replace(this.tagWithAttrs, (m) => {
+      for (const [from, to] of Object.entries(this.attrs)) {
+        const attribute = new RegExp(`${from}(\s*=)`, 'g');
+        m = m.replace(attribute, (f) => f.replace(from, to));
+      }
+
+      return m;
+    });
+  }
 }
 
 export default function transform(file, api, options) {
   const j = api.jscodeshift;
   const { tag, attrs, tabWidth = 4, useTabs = false } = options;
+  const tagAttrReplacer = new AttrReplacer(j, tag, attrs);
+  const { hasTag, tagWithNewAttrs } = tagAttrReplacer;
 
   return j(file.source)
     .find(j.TaggedTemplateExpression, htmlTaggedTemplateLiteral)
-    .forEach((path) => {
-      const source = j(path.node).toSource();
-      const result = replaceAttrs(source, tag, attrs);
-
-      if (source === result) {
-        return path;
-      }
-
-      j(path).replaceWith(result);
-    })
+    .filter(hasTag)
+    .replaceWith(tagWithNewAttrs)
     .toSource({ tabWidth, useTabs });
 }
