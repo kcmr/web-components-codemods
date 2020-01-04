7f8a5a7477bc5665e38ed9bbd992a437bef5ba2d
Kus Cámara <kcmr@users.noreply.github.com>
2020-01-04 19:50:46 +0100

    fix(run-transform): expand file paths with globs
    #4

--- src/run-transform.js
+++ src/run-transform.js
@@ -3,25 +3,35 @@
 const path = require('path');
 const execa = require('execa');
 const dargs = require('dargs');
-const transformsDir = path.join(__dirname, '../', 'transforms');
-const jscodeshiftExecutable = require.resolve('.bin/jscodeshift');
+const globby = require('globby');
+
+// Shamelessly stolen from https://github.com/reactjs/react-codemod/blob/master/bin/cli.js
+function expandFilePathsIfNeeded(files) {
+  const shouldExpandFiles = files.some((file) => file.includes('*'));
+  return shouldExpandFiles ? globby.sync(files) : files;
+}
 
 module.exports = function runTransform({ command, program, options }) {
-  const transformScript = path.join(transformsDir, `${command}.js`);
+  const transformScript = path.join(
+    __dirname,
+    '../transforms/',
+    `${command}.js`
+  );
   const excludes = ['files'];
+  const files = expandFilePathsIfNeeded([options.files]);
   const args = [
-    options.files,
-    '-t',
-    transformScript,
-    '--ignore-pattern=**/node_modules/**',
+    ...files,
+    `--transform=${transformScript}`,
+    '--ignore-pattern=**/node_modules/**,**/bower_components/**',
     ...dargs(options, { excludes }),
   ];
 
+  // always preview in dry-run mode
   if (options.dry) {
     args.push('--print');
   }
 
-  const result = execa.sync(jscodeshiftExecutable, args, {
+  const result = execa.sync('jscodeshift', args, {
     stdio: 'inherit',
   });
 
