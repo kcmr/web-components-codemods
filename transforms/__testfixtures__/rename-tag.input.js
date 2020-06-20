5b40bde86865b01981588b5f0f39f6f1131a89cb
Kus Cámara <kcmr@users.noreply.github.com>
2020-06-20 11:52:28 +0200

    feat(transforms): add a codemod to rename tags

---
+++ transforms/__testfixtures__/rename-tag.input.js
@@ -0,0 +1,10 @@
+const stringLiteral = `
+<some-tag-name>
+  <some-tag-name-two></some-tag-name-two>
+</some-tag-name>`;
+
+const taggedTemplate = html`<some-tag-name></some-tag-name>`;
+
+const stringMatch = document.querySelector('some-tag-name');
+
+const containsName = `<some-tag-name-two></some-tag-name-two>`;
---
+++ transforms/rename-tag.js
@@ -0,0 +1,36 @@
+class TagReplacer {
+  constructor(parser, oldTag, newTag) {
+    this.parser = parser;
+    this.oldTag = oldTag;
+    this.newTag = newTag;
+    this.tag = new RegExp(`${oldTag}[^-]`, 'g');
+    this.hasTag = this.nodeHasTag.bind(this);
+    this.renamedTag = this.getNewTag.bind(this);
+  }
+
+  nodeHasTag(path) {
+    return this.parser(path.node).toSource().match(this.tag);
+  }
+
+  getNewTag(path) {
+    const source = this.parser(path).toSource();
+    return this.replaceTag(source);
+  }
+
+  replaceTag(source) {
+    return source.replace(this.tag, (m) => m.replace(this.oldTag, this.newTag));
+  }
+}
+
+export default function transform(file, api, options) {
+  const j = api.jscodeshift;
+  const { oldTag, newTag, tabWidth = 2, useTabs = false } = options;
+  const tagReplacer = new TagReplacer(j, oldTag, newTag);
+  const { hasTag, renamedTag } = tagReplacer;
+
+  return j(file.source)
+    .find(j.TemplateElement)
+    .filter(hasTag)
+    .replaceWith(renamedTag)
+    .toSource({ tabWidth, useTabs });
+}
