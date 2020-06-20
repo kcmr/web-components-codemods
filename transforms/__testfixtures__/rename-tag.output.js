c405b3bb2f1c2e0ea722d7e87a4f100e782e9d37
Kus Cámara <kcmr@users.noreply.github.com>
2020-06-20 13:46:23 +0200

    fix(transforms): find tag name in any string

--- transforms/__testfixtures__/rename-tag.output.js
+++ transforms/__testfixtures__/rename-tag.output.js
@@ -6,6 +6,7 @@ const stringLiteral = `
 const taggedTemplate = html`<some-tag-name-renamed></some-tag-name-renamed>`;
 
 const stringMatch = document.querySelector('some-tag-name-renamed');
+const stringMatch2 = ["\"\\n      </some-tag-name-renamed>\\n    \""];
 
 const containsName = `<some-tag-name-two></some-tag-name-two>`;
 
